23dfa3b1c3f3e8adfc6513c490e28781d69a2a5a
LevyCostta <levicostasouza@hotmail.com>
2025-03-24 21:01:55 -0300

    Create 4_primeiro_decorador_acucar_sintax.py
    Para utilizar os decoradores de maneira mais simples, o python permite que voce utilize simbolos, isso é chamado de "Açúcar Sintático"

---
+++ Decoradores, Iteradores e Geradores com Python/decoradores/4_primeiro_decorador_acucar_sintax.py
@@ -0,0 +1,13 @@
+def meu_decorador(funcao):
+    def envelope():
+        print('Faz algo antes de executar a funcao')
+        funcao()
+        print('Faz algo depois de executar a funcao')
+
+    return envelope
+
+@meu_decorador
+def ola_mundo():
+    print('Olá Mundo!')
+
+ola_mundo()
