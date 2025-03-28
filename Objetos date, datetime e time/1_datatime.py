a6a2aec8f11d0838f8becbf24dd30ec3aa7a0f3b
LevyCostta <levicostasouza@hotmail.com>
2025-03-27 21:45:55 -0300

    Aprendendo a trabalhar com datas, horas e fusos horarios
    Aprendendo a utilzar o modulo "datetime"

---
+++ Objetos date, datetime e time/1_datatime.py
@@ -0,0 +1,14 @@
+from datetime import date, datetime, time
+
+data = date(2023, 7, 10)
+
+print(data)
+
+print(data.today())
+
+data_hora = datetime(2025, 3, 27, 21, 41, 20)
+print(data_hora)
+print(datetime.today())
+
+hora = time(21, 45, 0)
+print(hora)
