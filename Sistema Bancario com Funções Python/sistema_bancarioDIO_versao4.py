faed626b02ff4450e767174f99afbf7582e05d30
LevyCostta <levicostasouza@hotmail.com>
2025-05-12 18:17:31 -0300

    Iterador Perosnalizado sobre todas as contas, que retorna informações basicas de cada conta

--- Sistema Bancario com Funções Python/sistema_bancarioDIO_versao4.py
+++ Sistema Bancario com Funções Python/sistema_bancarioDIO_versao4.py
@@ -2,6 +2,30 @@ from datetime import datetime
 from abc import ABC, abstractmethod
 import textwrap
 
+class ContasIterador:
+    def __init__(self, contas):
+        self.contas = contas
+        self._index = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        try:
+            conta = self.contas[self._index]
+            return f"""\
+            Agência:\t{conta.agencia}
+            Número:\t\t{conta.numero}
+            Titular:\t{conta.cliente.nome}
+            Saldo:\t\tR$ {conta.saldo:.2f}
+        """
+
+        except IndexError:
+            raise StopIteration
+        finally:
+            self._index +=1
+        
+
 class Cliente:
     def __init__(self, endereco):
         self.endereco = endereco
@@ -340,7 +364,7 @@ def criar_conta(numero_conta, clientes, contas):
     print('\n ======== CONTA CRIADA COM SUCESSO ========')
 
 def listar_contas(contas):
-    for conta in contas:
+    for conta in ContasIterador(contas):
         print('=' * 100)
         print(textwrap.dedent(str(conta)))
 
