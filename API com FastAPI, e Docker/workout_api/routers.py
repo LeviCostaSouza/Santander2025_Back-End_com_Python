795febe1dda7daeec8d18b0a53bccef2a8fb5864
LevyCostta <levicostasouza@hotmail.com>
2025-06-09 19:05:56 -0300

    Criação das rotas de Centro de Treinamento

--- API com FastAPI, e Docker/workout_api/routers.py
+++ API com FastAPI, e Docker/workout_api/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter  
 from workout_api.atleta.controller import router as atleta
 from workout_api.categorias.controller import router as categorias
+from workout_api.centro_treinamentos.controller import router as centro_treinamentos
 
 api_router = APIRouter()
 api_router.include_router(atleta, prefix='/atletas', tags=['atletas'])
-api_router.include_router(categorias, prefix='/categorias', tags=['categorias'])
+api_router.include_router(centro_treinamentos, prefix='/centro_treinamentos', tags=['centro_treinamentos'])
